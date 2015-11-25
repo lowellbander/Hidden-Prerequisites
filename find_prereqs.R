3ae81e9950658c4161ae292f224cde0b9725b844
Lowell Bander <lowellbander@gmail.com>
2015-11-25 07:56:12 -0800

    finished createCoursesTakenHistogram()

--- find_prereqs.R
+++ find_prereqs.R
@@ -1,15 +1,30 @@
 setwd('~/gits/Hidden-Prerequisites/')
 
 load(file = 'ID_GPA_LIST.data')
+rownames(DF) = as.character(DF$ID);
 # DF contains a mapping of [hashed] UID's to final GPA's
 
 main = function() {
+  printMeta()
+  createGPAhist()
+  createCoursesTakenHistogram()
+}
+
+# creates a histogram showing the frequency of courses taken by a student
+createCoursesTakenHistogram = function() {
+  coursesTakenPerStudent = data.frame(numberOfCoursesTaken = sapply(listStud, length));  
+  rownames(coursesTakenPerStudent) = names(listStud);  
+  data = DF
+  data$numberOfCoursesTaken = coursesTakenPerStudent[ rownames(DF) , 'numberOfCoursesTaken' ]
+  #print(head(DF,10)); # sanity check
   
-  nrCoursesStud = sapply(listStud, getLen);
-  DFNRC = data.frame(nrC = nrCoursesStud);  rownames(DFNRC) = names(listStud);  print(head(DFNRC))
-  DF$nrC = DFNRC[ rownames(DF) , 'nrC' ]
-  print(head(DF,30));
+  # subset students only with > 5 courses taken
+  data = subset(data, numberOfCoursesTaken > 5)
   
+  pdf('HistNRCoursesTaken.pdf');
+  hist(data$numberOfCoursesTaken, breaks=20, main='Histogram of courses taken per student', ylab = ' Nr courses '); 
+  dev.off();
+  #print(sort(table(data$numberOfCoursesTaken)))
 }
 
 # prints meta information about the data
